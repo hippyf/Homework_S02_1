45b791a14f93ddccdad34c4ea803f1cfa8224cdb
yana sarvarova <serg.yana@mail.ru>
2022-07-01 00:37:02 +0300

    Дополнительная задача 2

---
+++ DopTask02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано трёхзначное число N. Определить кратна ли трём сумма всех его цифр.
+
+Console.WriteLine("Введите трёхзначное число N");
+int number = Convert.ToInt32(Console.ReadLine());
+DopTask02(number);
+
+void DopTask02 (int n)
+{
+    if (n >= 100 && n <= 999)
+    {
+        int sum = (n/100 + (n%100)/10 + n%10);
+        if (sum%3 == 0) Console.WriteLine ("Сумма цифр числа " + number + " кратна трём");
+        else Console.WriteLine ("Сумма цифр числа " + number + " не кратна трём");
+    }
+    else Console.WriteLine ("Введено неверное число");
+}
