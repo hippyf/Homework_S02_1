cc064bccc775e77b3ccca991c4b3ac5c948d14a5
yana sarvarova <serg.yana@mail.ru>
2022-07-01 01:25:51 +0300

    Дополнительная задача 4

---
+++ DopTask04/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дан массив длиной 10 элементов. Заполнить его последовательно числами от 1 до 10
+
+int [] array = new int[10];
+Task4(array);
+
+void Task4 (int[] array)
+{
+    int index = 0;
+    int i = 1;
+    while (index < array.Length)
+    {
+        array[index] = i;
+        Console.Write(array[index] + " ");
+        i++;
+        index++;
+    }
+    Console.WriteLine();
+}
