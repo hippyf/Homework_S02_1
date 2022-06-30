a96c3bdd3c3d3019a36dc9e634c00f6cc9e6a628
yana sarvarova <serg.yana@mail.ru>
2022-07-01 02:13:16 +0300

    Дополнительная задача 1

---
+++ DopTask01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* . Рассчитать значение y при заданном x 
+по формуле y = (sin^2 x) при x > 0 , 1 - 2*sin(x^2) в противном случае */
+
+Console.WriteLine("Введите значение x");
+double x = Convert.ToDouble(Console.ReadLine());
+DopTask01(x);
+
+void DopTask01 (double x)
+{
+    double y = 0;
+    if (x > 0)
+    {
+        y = Math.Pow(Math.Sin(x), 2);
+        Console.WriteLine ("y = " + y);
+    }
+    else
+    {
+        y = (1 - 2*(Math.Sin(x*x)));
+        Console.WriteLine ("y = " + y);
+    }
+}
